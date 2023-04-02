a4122b9ba3e1b68ee905d19c094fc0fe3e35f1fe
Juan Piero Santisteban Quiroz <jpsantq@gmail.com>
2023-04-02 18:05:22 -0500

    feat: Clase 07

---
+++ src/app/services/value.service.spec.ts
@@ -0,0 +1,43 @@
+import { ValueService } from './value.service';
+
+describe('ValueService', () => {
+  let service: ValueService;
+
+  beforeEach(() => {
+    service = new ValueService();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('Tests for getValue', () => {
+    it('should return value', () => {
+      const rta = service.getValue();
+      expect(rta).toBe('my value');
+    });
+  });
+
+  describe('Tests for setValue', () => {
+    it('should set value', () => {
+      expect(service.getValue()).toBe('my value');
+      service.setValue('new value');
+      const rta = service.getValue();
+      expect(rta).toBe('new value');
+    });
+  });
+
+  describe('Tests for getPromiseValue', () => {
+    it('should return value with then', (done) => {
+      service.getPromiseValue().then((rta) => {
+        expect(rta).toBe('value');
+        done();
+      });
+    });
+
+    it('should return value with async/await', async () => {
+      const rta = await service.getPromiseValue();
+      expect(rta).toBe('value');
+    });
+  });
+});
