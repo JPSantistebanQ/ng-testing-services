8460a658f5622d585d73dc7696c61989936c4b77
Juan Piero Santisteban Quiroz <jpsantq@gmail.com>
2023-04-02 17:07:34 -0500

    feat: Clase 03

---
+++ src/app/calculator.spec.ts
@@ -0,0 +1,24 @@
+import { Calculator } from './calculator';
+
+describe('Calculator', () => {
+    let calculator: Calculator;
+
+    beforeEach(() => {
+        calculator = new Calculator();
+    });
+
+    it('#multiply should multiply two numbers', () => {
+        const rta = calculator.multiply(3, 3);
+        expect(rta).toBe(9);
+    });
+
+    it('#multiply should divide two numbers', () => {
+        const rta = calculator.divide(3, 3);
+        expect(rta).toBe(1);
+    });
+
+    it('#multiply should return null when divide by zero', () => {
+        const rta = calculator.divide(3, 0);
+        expect(rta).toBeNull();
+    });
+});
