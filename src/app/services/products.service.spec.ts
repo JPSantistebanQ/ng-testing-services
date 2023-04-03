1f86267c2891f5480396a5b1d561ed9547df5dd3
Juan Piero Santisteban Quiroz <jpsantq@gmail.com>
2023-04-02 20:26:22 -0500

    feat: Clase 17

--- src/app/services/products.service.spec.ts
+++ src/app/services/products.service.spec.ts
@@ -68,5 +68,60 @@ describe('ProductService', () => {
       req.flush(mockData);
       httpController.verify();
     });
+
+    it('should return products list whit taxes', (done) => {
+      const mockData: Product[] = [
+        {
+          ...generateOneProduct(),
+          price: 100, // 100 * .19 = 19
+        },
+        {
+          ...generateOneProduct(),
+          price: 200, // 200 * .19 = 38
+        },
+        {
+          ...generateOneProduct(),
+          price: 0, //0 * .19 = 0
+        },
+        {
+          ...generateOneProduct(),
+          price: -100, // = 0
+        },
+      ];
+      service.getAll().subscribe((data) => {
+        expect(data[0].taxes).toEqual(19);
+        expect(data[1].taxes).toEqual(38);
+        expect(data[2].taxes).toEqual(0);
+        expect(data[3].taxes).toEqual(0);
+        done();
+      });
+
+      // * http configuration
+      const url = `${environment.API_URL}/api/v1/products`;
+      const req = httpController.expectOne(url);
+      req.flush(mockData);
+      httpController.verify();
+    });
+
+    it('should send query params width limit 10 offset 3', (doneFn) => {
+      //Arrange
+      const mockData: Product[] = generateManyProducts(3);
+      const limit = 10;
+      const offset = 3;
+      //Act
+      service.getAll(limit, offset).subscribe((data) => {
+        //Assert
+        expect(data.length).toEqual(mockData.length);
+        doneFn();
+      });
+      //http config
+      const url = `${environment.API_URL}/api/v1/products?limit=${limit}&offset=${offset}`;
+      const req = httpController.expectOne(url);
+      req.flush(mockData);
+      const params = req.request.params;
+      expect(params.get('limit')).toEqual(`${limit}`);
+      expect(params.get('offset')).toEqual(`${offset}`);
+      httpController.verify();
+    });
   });
 });
